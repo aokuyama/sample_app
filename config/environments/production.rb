8c60b20c48e27bf193c0a3275c316a6c2cb21782
Aokuyama <ao980s@gmail.com>
2019-04-11 06:58:12 +0000

    Add user microposts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'bmwopcatkvgxufqe.herokuapp.com'
+  host = ENV['HEROKU_HOST']
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
